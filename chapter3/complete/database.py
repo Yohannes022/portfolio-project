a6706fa04de3216e56eb050e757be7bcde0dd697
Yohannes <99507599+Yohannes022@users.noreply.github.com>
2025-04-24 09:40:27 +0000

    Creating the database configuration file

--- chapter3/complete/database.py
+++ chapter3/complete/database.py
@@ -1,1 +1,14 @@
 """Database configuration"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///.fantasy_data.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base
